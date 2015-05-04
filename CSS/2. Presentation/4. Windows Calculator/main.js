ecf7dd195a2e2b18c9d8c46c6eb03adc4431d829
apomarinov <apomarinov@gmail.com>
2015-05-04 21:48:48 +0300

    calc update

--- CSS/2. Presentation/4. Windows Calculator/main.js
+++ CSS/2. Presentation/4. Windows Calculator/main.js
@@ -5,21 +5,50 @@ $(document).ready(function(){
 
 	function draggableWindow(){
 		var self = this;
-		var startX = 0,
-			startY = 0;
+		var currentWindow = $(".draggable").closest('.window');
+
+		var isDragged = false;
+
+		var previousX = 0,
+			previousY = 0;
+		var currentX = 0,
+			currentY = 0;
 		var deltaX = 0,
 			deltaY = 0;
 
 		this.init = function(){
 			$(".draggable").on({
 				mousedown: function (event){
-					self.startX = event.pageX;
-					self.startY = event.pageY;
-				},
-				mousemove: function(event){
-					
+					self.previousX = event.pageX;
+					self.previousY = event.pageY;
+					self.isDragged = true;
+					console.log('start');
 				}
 			});
+
+			$(document).on({
+				mousemove: function(event){
+					if(!self.isDragged) {
+						return false;
+					}
+					console.log('move');
+					self.currentX = event.pageX;
+					self.currentY = event.pageY;
+					self.deltaX = Math.abs(self.previousX - self.currentX);
+					self.deltaY = Math.abs(self.previousY - self.currentY);
+					currentWindow.css({
+						'margin-left': self.deltaX + "px",
+						'margin-top': self.deltaY + "px"
+					});
+				},
+				mouseup: function(){
+					self.reset();
+				}
+			});
+		};
+
+		this.reset = function(){
+			self.isDragged = false;
 		};
 	};
 });
